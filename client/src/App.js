77d723df4afdb0512124fe8affd6d6f098ee003a
berkingurcan <berkingurcan@yandex.com>
2022-11-08 11:15:21 +0300

    column two

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import Editor, { useMonaco, loader } from "@monaco-editor/react";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="column">
+        <Editor
+          height="90vh"
+          theme="vs-dark"
+          defaultLanguage="typescript"
+          defaultValue= "// Some codes"
+        />
+      </div>
+      <div className="column">
+        <h2>
+          MODULE 0
+        </h2>
+      </div>
     </div>
   );
 }
